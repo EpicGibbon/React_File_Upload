66d8a496264bb93f624e6488f3881b69e20ca0d0
EpicGibbon <michant1997@gmail.com>
2020-09-03 17:55:44 -0400

    have initial upload bar with changing state

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import FileUpload from './Components/FileUpload';
 
-const App = () => {
-    <div className="App"></div>
-}
+const App = () => (
+    <div className='container mt-4'>
+        <h4 className='display-4 text-center mb-4'>
+            <i className='fab fa-react'></i> React File Upload
+        </h4>
+
+        <FileUpload />
+    </div>
+)
 
 export default App;
